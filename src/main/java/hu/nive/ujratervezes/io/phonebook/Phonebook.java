6d909549aec6a633dd0de5a8a35c8dd7512ff27a
morton2 <gab.marcell@gmail.com>
2021-04-29 12:10:49 +0200

    result

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,29 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+public class Phonebook {
+    public void exportPhonebook(Map<String, String> contacts, String output) throws IOException {
+        if (contacts == null) {
+            Files.deleteIfExists(Path.of(output));
+            throw new IllegalArgumentException("param cannot be null.");
+        }
+        if (output == null) {
+            throw new IllegalArgumentException("param cannot be null.");
+        }
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output), StandardCharsets.UTF_8))) {
+                for (Map.Entry<String, String> entry : contacts.entrySet()) {
+                    //if (entry.getKey() == null || entry.getValue() == null) {
+                    //    throw new IllegalArgumentException("param cannot be null.");
+                    //}
+                    writer.write(entry.getKey() + ": " + entry.getValue());
+                    ((BufferedWriter) writer).newLine();
+                }
+            } catch (IOException ex) {
+            }
+        }
+    }
